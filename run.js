0a876927754064f4208ea466ad935b0d6cbe1665
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2022-06-25 22:04:10 +0200

    handle another path, not just "/"

--- run.js
+++ run.js
@@ -10,8 +10,9 @@ const app = express();
 
 if (conf.trust_proxy) app.set('trust proxy', conf.trust_proxy)
 
-app.use(express.static(__dirname + '/public'));
-app.use('/javascripts', express.static(__dirname + '/node_modules/jquery/dist'));
+const base_path = new URL(conf.our_base_url).pathname
+app.use(base_path, express.static(__dirname + '/public'));
+app.use(base_path + '/javascripts', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 const store = conf.session_store.mongoUrl ? require('connect-mongo').create({
@@ -20,7 +21,8 @@ const store = conf.session_store.mongoUrl ? require('connect-mongo').create({
     ttl: conf.ticket_validity_seconds, // short ttl that will be 
 }) : throw_("unknown session_store") ;
 app.use(session({ 
-    store, 
+    store,
+    cookie: { path: base_path }, 
     resave: false, saveUninitialized: false,
     ...conf.session_store.options,
 }));
@@ -28,7 +30,7 @@ app.use(session({
 app.set('views', __dirname + '/views');
 app.set("view engine", "ejs");
 
-app.use(require('./lib/cas_server')());
+app.use(base_path, require('./lib/cas_server')());
 
 
 const port = conf.port || process.env.PORT || '3001'
